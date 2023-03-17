7e68d648aaa6e0f2791891312e8ba0b0f372ae62
duong_vannhieu <duong_vannhieu@co-mit.com>
2023-03-17 16:34:59 +0700

    Refactor: CountPairsWithGivenSum

--- arrays/CountPairsWithGivenSum.java
+++ arrays/CountPairsWithGivenSum.java
@@ -1,2 +1,26 @@
-package arrays;public class CountPairsWithGivenSum {
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountPairsWithGivenSum {
+    public static int countPairsWithGivenSum(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(k - arr[i])) {
+                count+=map.get(k - arr[i]);
+            }
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 1};
+        System.out.println(countPairsWithGivenSum(arr, 2));
+    }
 }
